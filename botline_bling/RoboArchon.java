8d42205b45670f027a36abe5a5201bd1144b9bbf
rrphys <ryan.rollings@gmail.com>
2016-01-05 17:17:59 -0500

    Archons can clump

--- botline_bling/RoboArchon.java
+++ botline_bling/RoboArchon.java
@@ -9,11 +9,9 @@ public class RoboArchon extends RobotPlayer
 	
 	public static void init() throws GameActionException
 	{
-		rallyLoc = null;
+		rallyLoc = null;	
 	}
-	
-	
-	
+
     //Build scout
     //Read signal queue to get location round 20
     	//Store number of buddy archons
@@ -34,40 +32,15 @@ public class RoboArchon extends RobotPlayer
 	{
 		Direction[] directions = {Direction.NORTH, Direction.NORTH_EAST, Direction.EAST, Direction.SOUTH_EAST,
                 Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST};
-//build scout
-		Direction dirToBuild;
-		MapLocation archonLoc = rc.getLocation();
-		//System.out.println((archonLoc.x + archonLoc.y) % 2);
-		if (((archonLoc.x + archonLoc.y) % 2) == 0){
-			//System.out.println("on black diag");			
-			for(int i=1; i<=7; i=i+2){
-				dirToBuild = directions[i];
-				if (rc.hasBuildRequirements(RobotType.SCOUT)){
-					if (rc.canBuild(dirToBuild, RobotType.SCOUT)) {
-						rc.build(dirToBuild, RobotType.SCOUT);
-						break;
-					}
-				}
-			}
-		} else {//archon is on white, build on 
-			//System.out.println("on white rook");
-			for(int i=0; i<=7; i=i+2){
-				dirToBuild = directions[i];
-				if (rc.hasBuildRequirements(RobotType.SCOUT)){
-					if (rc.canBuild(dirToBuild, RobotType.SCOUT)) {
-						rc.build(dirToBuild, RobotType.SCOUT);
-						break;
-					}
-				}
-			}
-		}
-//read beacon
-		if (rc.getRoundNum() > RoboScout.SIGNAL_ROUND)
+
+
+		//read beacon
+		if (rc.getRoundNum() == RoboScout.SIGNAL_ROUND)
 		{
 			// check for signals by a time we should have received some
 
 			int minID = 1000000;
-			
+
 			Signal[] sigs = rc.emptySignalQueue();
 			for (Signal s : sigs)
 			{
@@ -84,7 +57,7 @@ public class RoboArchon extends RobotPlayer
 					{
 						rallyLoc = loc;
 						minID = s.getID();
-						//Debug.setStringRR("rallyLocx" +rallyLoc.x + "rallyLocy" +rallyLoc.y);
+						//Debug.setStringRR(""+ minID);
 					}
 					//System.out.println(s.getID() + " " + s.getLocation() + " " + loc + " / MY LOC:" + here);
 					break;
@@ -92,50 +65,86 @@ public class RoboArchon extends RobotPlayer
 					break;
 				}
 			}
-
+		}
+		
+		//build scout
+		RobotType robotToBuild = RobotType.TURRET;
+		if (rc.getRoundNum() <= 10){
+			robotToBuild = RobotType.SCOUT;
+			Debug.setStringRR("set to scout");
+			Direction dirToBuild;
+			MapLocation archonLoc = rc.getLocation();
+			//System.out.println((archonLoc.x + archonLoc.y) % 2);
+			if (((archonLoc.x + archonLoc.y) % 2) == 0){
+				//System.out.println("on black diag");			
+				for(int i=1; i<=7; i=i+2){
+					Debug.setStringRR("black");
+					dirToBuild = directions[i];
+					if (rc.hasBuildRequirements(robotToBuild)){
+						if (rc.canBuild(dirToBuild, robotToBuild)) {
+							if (rc.isCoreReady()){
+								rc.build(dirToBuild, robotToBuild);
+							}
+							break;
+						}
+					}
+				}
+			} else {//archon is on white, build on 
+				//System.out.println("on white rook");
+				for(int i=0; i<=7; i=i+2){
+					Debug.setStringRR("white");
+					dirToBuild = directions[i];
+					if (rc.hasBuildRequirements(robotToBuild)){
+						if (rc.canBuild(dirToBuild, robotToBuild)) {
+							if (rc.isCoreReady()){
+								rc.build(dirToBuild, robotToBuild);
+							}
+							break;
+						}
+					}
+				}
+			}
+		}else if(rallyLoc == null){//then wait
+			//rc.yield();//replace later
+		}else if(here.distanceSquaredTo(rallyLoc) > 4){
 			Debug.setStringRR("rallyLocx" + rallyLoc.x + "rallyLocy" + rallyLoc.y);
 			NavSafetyPolicy safety = new SafetyPolicyAvoidAllUnits();
-			Nav.goTo(rallyLoc, safety);
+			if (rc.isCoreReady()){ 
+				Nav.goTo(rallyLoc, safety);
+			}
+		}else{
+			robotToBuild = RobotType.TURRET;
 
-//			MapLocation target = rallyLoc;
-			// now the rally location is in rallyLoc
-
-
-
-			//		int fate=100;
-			//        if (rc.isCoreReady()) {
-			//            if (fate < 800) {
-			//                // Choose a random direction to try to move in
-			//                Direction dirToMove = Direction.values()[fate % 8];
-			//                // Check the rubble in that direction
-			//                if (rc.senseRubble(rc.getLocation().add(dirToMove)) >= GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
-			//                    // Too much rubble, so I should clear it
-			//                    rc.clearRubble(dirToMove);
-			//                    // Check if I can move in this direction
-			//                } else if (rc.canMove(dirToMove)) {
-			//                    // Move
-			//                    rc.move(dirToMove);
-			//                }
-			//            } else {
-			//                // Choose a random unit to build
-			//                RobotType typeToBuild = RobotType.values()[fate % 8];
-			//                // Check for sufficient parts
-			//                if (rc.hasBuildRequirements(typeToBuild)) {
-			//                    // Choose a random direction to try to build in
-			//                    Direction dirToBuild = Direction.values()[rand.nextInt(8)];
-			//                    for (int i = 0; i < 8; i++) {
-			//                        // If possible, build in this direction
-			//                        if (rc.canBuild(dirToBuild, typeToBuild)) {
-			//                            rc.build(dirToBuild, typeToBuild);
-			//                            break;
-			//                        } else {
-			//                            // Rotate the direction to try
-			//                            dirToBuild = dirToBuild.rotateLeft();
-			//                        }
-			//                    }
-			//                }
-			//            }
-			//        }
+			Direction dirToBuild;
+			MapLocation archonLoc = rc.getLocation();
+			//System.out.println((archonLoc.x + archonLoc.y) % 2);
+			if (((archonLoc.x + archonLoc.y) % 2) == 0){
+				//System.out.println("on black diag");			
+				for(int i=1; i<=7; i=i+2){
+					dirToBuild = directions[i];
+					if (rc.hasBuildRequirements(robotToBuild)){
+						if (rc.canBuild(dirToBuild, robotToBuild)) {
+							if (rc.isCoreReady()){
+								rc.build(dirToBuild, robotToBuild);
+							}
+							break;
+						}
+					}
+				}
+			} else {//archon is on white, build on 
+				//System.out.println("on white rook");
+				for(int i=0; i<=7; i=i+2){
+					dirToBuild = directions[i];
+					if (rc.hasBuildRequirements(robotToBuild)){
+						if (rc.canBuild(dirToBuild, robotToBuild)) {
+							if (rc.isCoreReady()){
+								rc.build(dirToBuild, robotToBuild);
+							}
+							break;
+						}
+					}
+				}
+			}
 		}
 	}
 }
