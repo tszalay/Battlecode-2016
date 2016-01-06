17e94911e1292d278c1c618d8813ec8472f40a93
sjfleming <sjf11@case.edu>
2016-01-05 21:45:22 -0500

    First commit of outline of Micro methods
    Skeleton that I hope to fill in soon, the preliminary logic has been
    thought through.

--- botline_bling/Micro.java
+++ botline_bling/Micro.java
@@ -4,13 +4,16 @@ import battlecode.common.*;
 
 public class Micro extends RobotPlayer
 {
+	public static Direction dirToZombies = null;
+	public static Direction dirToEnemies = null;
+	
 	public static boolean tryAttackSomebody() throws GameActionException
 	{
 		// can we even shoot bro?
 		if (!rc.isWeaponReady())
 			return false;
 		
-		// check zombies first, then enemies
+		// check zombies first, then enemies (priority is zombies, even over enemy archons...)
 		RobotInfo[] nearbyEnemies = rc.senseNearbyRobots(rc.getType().attackRadiusSquared, Team.ZOMBIE);
 		if (nearbyEnemies.length == 0)
 			nearbyEnemies = rc.senseNearbyRobots(rc.getType().attackRadiusSquared, theirTeam);
@@ -19,21 +22,55 @@ public class Micro extends RobotPlayer
 		if (nearbyEnemies.length == 0)
 			return false;
 		
-		RobotInfo bestTarget = null;
-		
-		for (RobotInfo ri : nearbyEnemies)
-		{
-			if (bestTarget == null)
-			{
-				bestTarget = ri;
-				continue;
-			}
-			
-			if (ri.health < bestTarget.health)
-				bestTarget = ri;
+		// if there is somebody to shoot at, pick the bot with the least health
+		RobotInfo bestTarget = nearbyEnemies[0];
+		for (RobotInfo enemy : nearbyEnemies)
+		{	
+			if (enemy.health < bestTarget.health)
+				bestTarget = enemy;
 		}
 		
 		rc.attackLocation(bestTarget.location);
+		return true;
+	}
+	
+	public static void doAvoidBeingKilled() throws GameActionException
+	{
+		
+	}
+	
+	public static void doAvoidDyingInfectedAtAnyCost() throws GameActionException
+	{
+		
+	}
+	
+	public static boolean doKiteZombies() throws GameActionException
+	{
+		
+		return true;
+	}
+	
+	public static boolean amOverpowered() throws GameActionException
+	{
+		
+		return true;
+	}
+	
+	public static boolean canWin1v1() throws GameActionException
+	{
+		
+		return true;
+	}
+	
+	public static boolean canSurviveCurrentSkirmish() throws GameActionException
+	{
+		
+		return true;
+	}
+	
+	public static boolean willDieInfected() throws GameActionException
+	{
+		
 		return true;
 	}
 	
