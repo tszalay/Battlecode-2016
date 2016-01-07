1e86304176e19c7cee3a82307e2e51ce7de70f4a
sjfleming <sjf11@case.edu>
2016-01-07 15:12:12 -0500

    Scout: updates

--- botline_bling/RoboScout.java
+++ botline_bling/RoboScout.java
@@ -9,6 +9,7 @@ public class RoboScout extends RobotPlayer
 	public static final int SIGNAL_ROUND = 30; 
 	public static MapLocation target = null;
 	public static boolean isFreeScout = false;
+	public static MapLocation rally = null;
 	
 	public static void init() throws GameActionException
 	{
@@ -18,23 +19,26 @@ public class RoboScout extends RobotPlayer
 		{
 			if (myArchon != null)
 			{
-				Message.sendMessageSignal(10000, MessageType.SPAWN, myArchon.location);
+				Message.sendMessageSignal(100, MessageType.SPAWN, myArchon.location);
 			}
 		}
 		target = here.add(rand.nextInt(200)-100,rand.nextInt(200)-100);
 		
-		// =========================================================
-		// for now, a hacked way of deciding if you are a free scout
-		Micro.updateAllies();
-		int numOtherScouts = 0;
-		for (RobotInfo bot : Micro.nearbyAllies)
-		{
-			if (bot.type == RobotType.SCOUT)
-				numOtherScouts += 1;
-		}
-		if (numOtherScouts >= 1)
-			isFreeScout = true;
-		// =========================================================
+//		// =========================================================
+//		// for now, a hacked way of deciding if you are a free scout
+//		Micro.updateAllies();
+//		int numOtherScouts = 0;
+//		for (RobotInfo bot : Micro.nearbyAllies)
+//		{
+//			if (bot.type == RobotType.SCOUT)
+//				numOtherScouts += 1;
+//		}
+//		if (numOtherScouts >= 1)
+//			isFreeScout = true;
+//		// =========================================================
+		isFreeScout = false;
+		
+		rally = here.add(0,1);
 			
 	}
 	
@@ -50,9 +54,25 @@ public class RoboScout extends RobotPlayer
 	
 	public static void doTurtleScout() throws GameActionException
 	{
-		// go to edge of turtle
-		
+		// go to edge of turtle by moving away from rally location but staying in turtle
+		NavSafetyPolicy safety = new SafetyPolicyAvoidAllUnitsAndStayInTurtle();
+		int numTurretsAdjacent = getNumTurretsAdjacent(here);
+		if (numTurretsAdjacent != 3) // we're not at the edge
+			Nav.goTo(here.add(here.directionTo(rally).opposite()), safety);
+		Debug.setStringSJF("num turrets adjacent = " + numTurretsAdjacent);
 	}
+	
+	public static int getNumTurretsAdjacent(MapLocation loc)
+    {
+    	RobotInfo units[] = rc.senseNearbyRobots(loc, 3, ourTeam);
+    	int numTurretsAdjacent = 0;
+    	for (RobotInfo friend : units)
+    	{
+    		if (friend.type == RobotType.TURRET)
+    			numTurretsAdjacent += 1;
+    	}
+    	return numTurretsAdjacent;
+    }
 	
 	public static void turn() throws GameActionException
 	{
@@ -61,13 +81,13 @@ public class RoboScout extends RobotPlayer
 		
 		if (rc.isCoreReady())
         {
-//        	if (!Micro.tryAvoidBeingKilled()) // avoidance micro
-//        	{
-//	        	if (isFreeScout)
-//	        		doFreeScout();
-//	        	else
-//	        		doTurtleScout();
-//        	}
+        	if (!Micro.tryAvoidBeingShot()) // avoidance micro
+        	{
+	        	if (isFreeScout)
+	        		doFreeScout();
+	        	else
+	        		doTurtleScout();
+        	}
         }
 	}
 }
