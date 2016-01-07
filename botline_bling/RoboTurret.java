ccff538a0275fd5270f55072046163e0a09447f9
kuanawanda <akuan@seas.harvard.edu>
2016-01-06 20:54:02 -0500

    Turtle pattern fixed
    Except one question about random locations.

--- botline_bling/RoboTurret.java
+++ botline_bling/RoboTurret.java
@@ -7,17 +7,19 @@ import battlecode.common.*;
 public class RoboTurret extends RobotPlayer
 {
 	static MapLocation unpackLoc=null;
-	static final int[] unpackSearchTableX = {-1,1,1,-1,-2,0,2,0,-2,-2,2,2,1,-1,-3,-3,-1,1};
-	static final int[] unpackSearchTableY = {1, 1, -1, -1, 0, 2, 0 ,-2,-2,2,2,-2,-3,-3,-1,1,3,3};
+	static final int[] unpackSearchTableX = {0,1,0,-1,-1,1,2,2,1,-1,-2,-2,-2,0,2,3,3,3,2,0,-2,-3,-3,-3,-1,-1,4,4,1,-1,-4,-4};
+	static final int[] unpackSearchTableY = {1,0,-1,0,2,2,1,1,-2,-2,-1,1,3,3,3,2,0,-2,-3,-3,-3,-2,0,2,4,4,1,-1,-4,-4,-1,1};
 
 	// AK: unpack search in this pattern
-	// [][]18[]18[][]
-	// []10[]06[]11[]
-	// 16[]01[]02[]19
-	// []05[]XX[]07[]
-	// 15[]04[]03[]20
-	// []09[]08[]12[]
-	// [][]14[]13[][]
+	// [][][]25[]26[][][]
+	// [][]13[]14[]15[][]
+	// []24[]05[]06[]16[]
+	// 32[]12[]01[]07[]27
+	// []23[]04{}02[]17[]
+	// 31[]11[]03[]08[]28
+	// []22[]10[]09[]18[]
+	// [][]21[]20[]19[][]
+	// [][][]30[]29[][][]
 	
 	public static void init() throws GameActionException
 	{
@@ -84,26 +86,33 @@ public class RoboTurret extends RobotPlayer
 
 	public static void turnTTM() throws GameActionException
 	{
+		
 		if (MapUtil.isLocOdd(rc.getLocation())){
 			rc.unpack();
 		}
 		
-		else if (unpackLoc == null) {
-			unpackLoc = findNewUnpackLoc();
-		}
-		else if (rc.getLocation() != unpackLoc){
+		else {
 			if (rc.isCoreReady()) {
+
 				RobotInfo[] nearbyEnemies = rc.senseNearbyRobots(rc.getType().sensorRadiusSquared, theirTeam);
 				RobotInfo[] nearbyZombies = rc.senseNearbyRobots(rc.getType().sensorRadiusSquared, Team.ZOMBIE);
 				NavSafetyPolicy safety = new SafetyPolicyAvoidAllUnits(nearbyEnemies, nearbyZombies);
+				// if ready to move, update closest unpack location and nav there
 				unpackLoc = findNewUnpackLoc();
+				
+				// If you failed, try to move to a random location
+				if (unpackLoc == null) {
+					Nav.goTo(rc.getLocation().add(rand.nextInt(200)-100,rand.nextInt(200)-100), safety);
+				}
+				
+				// Otherwise go towards the best location
+				else {
 				Nav.goTo(unpackLoc, safety);
+				}
 			}
 		}
-		else {
-			// you've reached your destination!
-			rc.unpack();
-		}
+
+
 	}
 
 
@@ -111,16 +120,14 @@ public class RoboTurret extends RobotPlayer
 	// AK find new unpack location
 	public static MapLocation findNewUnpackLoc() throws GameActionException
 	{
-		MapLocation newUnpackLoc = rc.getLocation();
-		if (!MapUtil.isLocOdd(newUnpackLoc)) {
-			newUnpackLoc = newUnpackLoc.add(Direction.WEST); // now will be odd
-		}
+		MapLocation myLoc = rc.getLocation();
+		MapLocation newUnpackLoc = myLoc;
 		
-		for (int i = 0; i < 18; i++) {
+		for (int i = 0; i < 32; i++) {
 			if (rc.isLocationOccupied(newUnpackLoc) || rc.senseRubble(newUnpackLoc) >= GameConstants.RUBBLE_SLOW_THRESH || !rc.onTheMap(newUnpackLoc)) {
-				newUnpackLoc = newUnpackLoc.add(unpackSearchTableX[i],unpackSearchTableY[i]);
+				newUnpackLoc = myLoc.add(unpackSearchTableX[i],unpackSearchTableY[i]);
 			}
-			else if (i == 18) {
+			else if (i == 32) {
 				newUnpackLoc = null; // For now, if you can't find a close location just give up
 			}
 			else {
