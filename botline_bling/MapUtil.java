f2a70ba082f680c4090221ff339287862b5ae351
rrphys <ryan.rollings@gmail.com>
2016-01-07 19:57:37 -0500

    MapUtil: random search dir, make a ball-ish

--- botline_bling/MapUtil.java
+++ botline_bling/MapUtil.java
@@ -9,7 +9,7 @@ public class MapUtil extends RobotPlayer
 	private static final int[] oddLocOffX = {-1,-1,1,1,-2,0,0,2,-2,-2,2,2,-3,-3,-1,-1,1,1,3,3,-4,0,0,4,-3,-3,3,3,-4,-4,-2,-2,2,2,4,4,-5,-5,-1,-1,1,1,5,5,-4,-4,4,4,-5,-5,-3,-3,3,3,5,5,-6,0,0,6,-6,-6,-2,-2,2,2,6,6,-7,-7,-5,-5,-1,-1,1,1,5,5,7,7,-6,-6,-4,-4,4,4,6,6};
 	private static final int[] oddLocOffY = {-1,1,-1,1,0,-2,2,0,-2,2,-2,2,-1,1,-3,3,-3,3,-1,1,0,-4,4,0,-3,3,-3,3,-2,2,-4,4,-4,4,-2,2,-1,1,-5,5,-5,5,-1,1,-4,4,-4,4,-3,3,-5,5,-5,5,-3,3,0,-6,6,0,-2,2,-6,6,-6,6,-2,2,-1,1,-5,5,-7,7,-7,7,-5,5,-1,1,-4,4,-6,6,-6,6,-4,4};
 
-    
+	
 	// AK check if map location is odd or even.
 	public static boolean isLocOdd(MapLocation loc) {
 		return ((loc.x+loc.y)&1)==1;
@@ -44,7 +44,7 @@ public class MapUtil extends RobotPlayer
 		
 		int[] dx = isodd ? oddLocOffX : evenLocOffX;
 		int[] dy = isodd ? oddLocOffY : evenLocOffY;
-		
+				
 		int finalind;
 		
 		switch (rc.getType())
@@ -53,23 +53,26 @@ public class MapUtil extends RobotPlayer
 			finalind = dx.length;
 			break;
 		case ARCHON:
-			finalind = isodd ? 57:53;
+			finalind = isodd ? 56:52;
 			break;
 		default:
-			finalind = isodd ? 37:33;
+			finalind = isodd ? 36:32;
 			break;
 		}
+
+		//randomize start search direction for first shell
+		int randFlipX = rand.nextBoolean() ? -1:1;
+		int randFlipY = rand.nextBoolean() ? -1:1;
 		
 		for (int i=0; i<finalind; i++)
 		{
-			MapLocation testloc = here.add(dx[i],dy[i]);
+			MapLocation testloc = here.add(dx[i]*randFlipX,dy[i]*randFlipY);
 			if (isGoodTurtle(testloc))
 			{
 				Debug.setStringTS("Last Turtle " + testloc);
 				return testloc;
 			}
 		}
-		
 		return null;
 	}
 }
