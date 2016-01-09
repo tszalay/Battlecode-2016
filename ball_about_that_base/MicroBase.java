72b3e2de0ac6c9546c123e0de09f0f142d9a39a3
Tamas Szalay <tamas@seas.harvard.edu>
2016-01-09 16:48:33 -0500

    Patched up all of Steve's mistakes I think

--- ball_about_that_base/MicroBase.java
+++ ball_about_that_base/MicroBase.java
@@ -1,26 +1,20 @@
 package ball_about_that_base;
 
-import java.util.ArrayList;
-
 import battlecode.common.*;
 
 public class MicroBase extends RobotPlayer
 {
 	public RobotInfo[] nearbyEnemies = null;
 	public RobotInfo[] nearbyZombies = null;
+	public RobotInfo[] nearbyHostiles = null;
 	public RobotInfo[] nearbyAllies = null;
 	
 	public DirectionSet canMoveDirs = null;
 	public DirectionSet safeMoveDirs = null;
-	public DirectionSet reallySafeMoveDirs = null;
-	public DirectionSet canBeSeenDirs = null;
+	public DirectionSet noTurretMoveDirs = null;
 	
-	public DirectionSet oddDirs = null;
-	
-	public Direction closestZombieDir = null;
-	public Direction closestEnemyDir = null;
-	public Direction bestEscapeDir = null;
-	
+	private int[] distToClosestHostile = null;
+
 	
 	public MicroBase()
 	{
@@ -53,244 +47,153 @@ public class MicroBase extends RobotPlayer
 		return nearbyZombies;
 	}
 	
-	/*	
-	public static void updateAllies() throws GameActionException
+	public RobotInfo[] getNearbyHostiles()
 	{
-		// find allies within sensor range
-		nearbyAllies = rc.senseNearbyRobots(rc.getType().sensorRadiusSquared, ourTeam);
+		if (nearbyHostiles != null)
+			return nearbyHostiles;
 		
-		// go though and tally up firepower
-		RobotInfo me = rc.senseRobot(rc.getID());
-		allyTotalDamagePerTurn = ( me.attackPower / (me.type.attackDelay + 1) );
-		allyTotalHealth = me.health;
-		if (nearbyAllies.length > 0)
+		nearbyHostiles = rc.senseHostileRobots(here, rc.getType().sensorRadiusSquared);
+		return nearbyHostiles;
+	}
+	
+	// this gets called by other functions to compile various stats and info
+	// before they return the computed values
+	private void computeSafetyStats()
+	{
+		// already done
+		if (distToClosestHostile != null)
+			return;
+		
+		RobotInfo[] nearby = getNearbyHostiles();
+		
+		// this is used for best retreat direction calculation
+		distToClosestHostile = new int[9];
+		
+		safeMoveDirs = new DirectionSet();
+		noTurretMoveDirs = new DirectionSet();
+
+		// only check directions we can actually move
+		for (Direction d : getCanMoveDirs().getDirections())
 		{
-			for (RobotInfo friend : nearbyAllies)
+			MapLocation testloc = here.add(d);
+			
+			int closestDistSq = 1000;
+			boolean isThisSquareSafe = true;
+			boolean isThisSquareTurretSafe = true;
+			
+			for (RobotInfo ri : nearby)
 			{
-				allyTotalDamagePerTurn += ( friend.attackPower / (friend.type.attackDelay + 1) );
-				allyTotalHealth += friend.health;
+				int distSq = testloc.distanceSquaredTo(ri.location);
+				if (distSq < closestDistSq)
+					closestDistSq = distSq;
+
+				if (distSq <= ri.type.attackRadiusSquared)
+				{
+					isThisSquareSafe = false;
+					if (ri.type == RobotType.TURRET)
+						isThisSquareTurretSafe = false;
+				}
+
 			}
+			
+			distToClosestHostile[d.ordinal()] = closestDistSq;
+			
+			if (isThisSquareSafe)
+				safeMoveDirs.add(d);
+			if (isThisSquareTurretSafe)
+				noTurretMoveDirs.add(d);
 		}
 	}
 	
-	public static boolean tryMove(MapLocation loc, NavSafetyPolicy safety) throws GameActionException
+	// compute which direction moves us the farthest from the closest enemy
+	// (trying to use safe squares first)
+	public Direction getBestEscapeDir()
 	{
-		Nav.goTo(loc, safety);
-		if (here.equals(rc.getLocation())) // we didn't move
+		DirectionSet dirs = getSafeMoveDirs();
+		
+		// if there are no safe moves, just do something
+		if (!dirs.any())
+			dirs = getCanMoveDirs();
+		
+		if (!dirs.any())
+			return Direction.NONE;
+		
+		Direction bestDir = null;
+		int distToClosest = 0;
+		
+		for (Direction d : dirs.getDirections())
+		{
+			if (distToClosestHostile[d.ordinal()] > distToClosest)
+			{
+				distToClosest = distToClosestHostile[d.ordinal()];
+				bestDir = d;
+			}
+		}
+		
+		return bestDir;
+	}
+
+	public DirectionSet getNoTurretMoveDirs()
+	{
+		computeSafetyStats();
+		return noTurretMoveDirs;
+	}
+
+	public DirectionSet getSafeMoveDirs()
+	{
+		computeSafetyStats();
+		return safeMoveDirs;
+	}
+	
+	// get directions we can move in
+	public DirectionSet getCanMoveDirs()
+	{
+		if (canMoveDirs != null)
+			return canMoveDirs;
+		
+		canMoveDirs = DirectionSet.makeStay();
+		
+		for (Direction d : Direction.values())
+			if (rc.canMove(d))
+				canMoveDirs.add(d);
+		
+		return canMoveDirs;
+	}
+	
+	public boolean isInDanger()
+	{
+		return getNearbyHostiles().length > 0;
+	}
+	
+	public boolean tryMove(Direction d) throws GameActionException
+	{
+		// don't do anything, but don't throw error, this is ok
+		if (d == Direction.NONE)
 			return false;
+		
+		// double check!
+		if (d != null && rc.canMove(d) && rc.isCoreReady())
+		{
+			rc.move(d);
+			return true;
+		}
 		else
-			return true;
-	}
-	
-	public static boolean tryRetreat() throws GameActionException
-	{
-		if (!rc.isCoreReady())
-			return false;
-		
-		NavSafetyPolicy safety = new SafetyPolicyRecklessAbandon();
-		
-		// get the retreat direction
-		Direction retreatDir = null;
-		if (dirToClosestEnemy == Direction.NONE && dirToClosestZombie == Direction.NONE) // no attackers
-			return false;
-		
-		// retreat if we can within the bounds of the safety policy
-		MapLocation retreatLoc = here.add(dirToClosestEnemy.opposite()).add(dirToClosestZombie.opposite());
-		return (tryMove(retreatLoc, safety));
-	}
-	
-	public static boolean tryRushEnemies() throws GameActionException
-	{
-		// bum rush enemies in sight range while avoiding zombies
-		NavSafetyPolicy safety = new SafetyPolicyAvoidZombies(nearbyZombies);
-		if (dirToClosestEnemy != null)
-			return tryMove(here.add(dirToClosestEnemy), safety);
-		
-		return false;
-	}
-	
-	public static boolean tryAttackBot(RobotInfo targetBot) throws GameActionException
-	{
-		if (targetBot == null)
-			return false;
-		if (rc.canAttackLocation(targetBot.location))
 		{
-			rc.attackLocation(targetBot.location);
-			return true;
-		}
-		return false;
-	}
-	
-	public static boolean tryAttackSomebody() throws GameActionException
-	{
-		// try to attack someone in weapons range
-		// this method SHOULD BE CALLED AFTER CALLING collateNearbyRobotInfo()
-		// this method can be called from outside without doing anything first, it will just default to shooting at bot with least health
-		
-		// can we even shoot bro?
-		if (!rc.isWeaponReady())
-			return false;
-		
-		// use the info we've collated
-		// priority: enemy archon, zombies, enemies
-		if (tryAttackBot(enemyPriorityTarget))
-			return true;
-		if (tryAttackBot(lowestHealthZombie))
-			return true;
-		if (tryAttackBot(lowestHealthEnemy))
-			return true;
-		
-		// if this is called from outside Micro.java, and the targets are not set, just default to this:
-		RobotInfo[] localEnemies = rc.senseHostileRobots(here, rc.getType().attackRadiusSquared);
-		if (localEnemies.length == 0)
-			return false;
-		// find enemy with lowest health and try to attack
-		RobotInfo target = localEnemies[0];
-		for (RobotInfo bot : localEnemies)
-		{
-			if (bot.health < target.health)
-				target = bot;
-		}
-		if (tryAttackBot(target))
-			return true;
-		
-		return false;
-	}
-	
-	private static void collateNearbyRobotInfo() throws GameActionException
-	{
-		// loop through zombies
-		zombieTotalDamagePerTurn = 0;
-		zombieTotalHealth = 0;
-		lowestHealthZombie = null;
-		closestZombie = null;
-		
-		dirToClosestZombie = Direction.NONE;
-		inZombieSensorRange = false;
-		if (nearbyZombies.length>0)
-		{
-			closestZombie = nearbyZombies[0];
-			lowestHealthZombie = nearbyZombies[0];
-			for (RobotInfo zombie : nearbyZombies)
+			System.out.println("Movement exception: tried to move but couldn't!");
+			if (d == null)
 			{
-				// update the closest zombie
-				if (here.distanceSquaredTo(zombie.location) < here.distanceSquaredTo(closestZombie.location))
-					closestZombie = zombie;
-				
-				// add up the firepower of relevant zombies
-				if (here.distanceSquaredTo(zombie.location) <= zombie.type.attackRadiusSquared)
-					zombieTotalDamagePerTurn += (zombie.attackPower / (zombie.type.attackDelay+1));
-				
-				// keep track of the zombie with lowest health
-				if (zombie.health < lowestHealthZombie.health)
-					lowestHealthZombie = zombie;
-				
-				// check if we are in zombie sensor range
-				if (inZombieSensorRange == false && here.distanceSquaredTo(zombie.location) <= zombie.type.sensorRadiusSquared)
-					inZombieSensorRange = true;
-				
-				// add up total health
-				zombieTotalHealth += zombie.health;
+				System.out.println("Reason: null direction");
+				return false;
 			}
+			if (!rc.isCoreReady())
+				System.out.println("Reason: core not ready");
+			if (rc.isLocationOccupied(here.add(d)))
+				System.out.println("Reason: location occupied");
+			if (rc.senseRubble(here.add(d)) > GameConstants.RUBBLE_OBSTRUCTION_THRESH)
+				System.out.println("Reason: too much rubble");
 			
-			// figure out how long until closest zombie attacks
-			RobotInfo[] alliesInClosestZombieSightRange = rc.senseNearbyRobots(closestZombie.location, closestZombie.type.sensorRadiusSquared, ourTeam);
-			if (alliesInClosestZombieSightRange.length > 0)
-			{
-				// loop through to find the ally that the closest zombie will attack
-				RobotInfo allyTargetOfClosestZombie = alliesInClosestZombieSightRange[0];
-				for (RobotInfo allyTarget : alliesInClosestZombieSightRange)
-				{
-					if (closestZombie.location.distanceSquaredTo(allyTarget.location) < closestZombie.location.distanceSquaredTo(allyTargetOfClosestZombie.location))
-						allyTargetOfClosestZombie = allyTarget;
-				}
-				
-				if (rc.getID() != allyTargetOfClosestZombie.ID)
-				{
-					// if the closest zombie's target is not me, calculate how many turns it will be engaged with them
-					turnsUntilFirstZombieAttacks = (int) ( allyTargetOfClosestZombie.health / (closestZombie.attackPower / (closestZombie.type.attackDelay+1)) );
-				}
-			}
-			
-			// compute direction to closest zombie
-			dirToClosestZombie = here.directionTo(closestZombie.location);
+			return false;
 		}
-		
-		// loop through enemy team
-		enemyTotalDamagePerTurn = 0;
-		enemyTotalHealth = 0;
-		lowestHealthEnemy = null;
-		closestEnemy = null;
-		dirToClosestEnemy = Direction.NONE;
-		enemyViperInSightRange = false;
-		if (nearbyEnemies.length>0)
-		{
-			// reset priority target
-			enemyPriorityTarget = null;
-			closestEnemy = nearbyEnemies[0];
-			lowestHealthEnemy = nearbyEnemies[0];
-			
-			// go through enemies, find targets, count up enemy firepower
-			for (RobotInfo enemy : nearbyEnemies)
-			{
-				// look for archon enemy targets
-				if (rc.getType().attackRadiusSquared >= here.distanceSquaredTo(enemy.location))
-				{
-					if (enemy.type == RobotType.ARCHON)
-						enemyPriorityTarget = enemy;
-				}
-				
-				// update the closest enemy
-				if (here.distanceSquaredTo(enemy.location) < here.distanceSquaredTo(closestEnemy.location))
-					closestEnemy = enemy;
-				
-				// count up enemy firepower
-				if (here.distanceSquaredTo(enemy.location) <= enemy.type.attackRadiusSquared)
-					enemyTotalDamagePerTurn += (enemy.attackPower / (enemy.type.attackDelay+1));
-				
-				// keep track of the enemy with lowest health
-				if (enemy.health < lowestHealthEnemy.health)
-					lowestHealthEnemy = enemy;
-				
-				// check for vipers
-				if (enemy.type == RobotType.VIPER)
-					enemyViperInSightRange = true;
-				
-				// add up total health
-				enemyTotalHealth += enemy.health;
-			}
-			
-			// compute direction to closest enemy
-			dirToClosestEnemy = here.directionTo(closestEnemy.location);
-			
-		}
-		
-		//Debug.setStringSJF("enemy dir = " + dirToClosestEnemy.toString() + ", zombie dir = " + dirToClosestZombie.toString());
-		
-		// figure out if i am in shooting range of anyone
-		boolean enemyInRange = ( closestEnemy != null && here.distanceSquaredTo(closestEnemy.location) <= closestEnemy.type.attackRadiusSquared );
-		boolean zombieInRange = ( closestZombie != null && here.distanceSquaredTo(closestZombie.location) <= closestZombie.type.attackRadiusSquared );
-		if (enemyInRange || zombieInRange)
-			amISafe = false;
-		else
-			amISafe = true;
-	}
-	*/
-	public static RobotInfo getClosestRobot(RobotInfo[] nearby, MapLocation loc)
-	{
-		int minsqdist = 1000;
-		RobotInfo closest = null;
-		
-		for (RobotInfo ri : nearby)
-		{
-			if (ri.location.distanceSquaredTo(loc) < minsqdist)
-			{
-				closest = ri;
-				minsqdist = ri.location.distanceSquaredTo(loc);
-			}
-		}
-		
-		return closest;
 	}
 	
 	public static RobotInfo getClosestTurretTarget(RobotInfo[] nearby, MapLocation loc)
@@ -346,5 +249,81 @@ public class MicroBase extends RobotPlayer
 		Debug.setStringTS("AA" + new MapLocation(xtot,ytot));
 		
 		return new MapLocation(xtot,ytot);
+	}
+	
+	public static RobotInfo getClosestUnitTo(RobotInfo[] nearby, MapLocation loc)
+	{
+		if (nearby.length == 0)
+			return null;
+		
+		RobotInfo closest = null;
+		int closestDistSq = 1000;
+		
+		for (RobotInfo ri : nearby)
+		{
+			int distSq = loc.distanceSquaredTo(ri.location);
+			if (distSq < closestDistSq)
+			{
+				closest = ri;
+				closestDistSq = distSq;
+			}
+		}
+		
+		return closest;
+	}
+	
+	
+	public boolean tryAttackSomeone() throws GameActionException
+	{
+		RobotInfo target = null;
+		
+		RobotInfo[] zombies = this.getNearbyZombies();
+		if (zombies==null || zombies.length==0)
+		{
+			RobotInfo[] enemies = this.getNearbyEnemies();
+			if (enemies==null || enemies.length==0)
+			{
+				return false;
+			}
+			else
+			{
+				target = getClosestUnitTo(enemies, here);
+			}
+		}
+		else
+		{
+			target = getClosestUnitTo(zombies, here);
+		}
+		if (target != null && rc.canAttackLocation(target.location))
+		{
+			rc.attackLocation(target.location);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean tryAvoidBeingShot() throws GameActionException
+	{
+		if (!rc.isCoreReady())
+			return this.tryAttackSomeone();
+		
+		// hack for now: just move directly away from COM
+		MapLocation enemyLoc = this.getEnemyCOM();
+		MapLocation zombieLoc = this.getZombieCOM();
+		
+		if (rc.canMove(here.directionTo(zombieLoc).opposite()))
+		{
+			rc.move(here.directionTo(zombieLoc).opposite());
+			return true;
+		}
+		
+		if (rc.canMove(here.directionTo(enemyLoc).opposite()))
+		{
+			rc.move(here.directionTo(enemyLoc).opposite());
+			return true;
+		}
+		
+		return this.tryAttackSomeone();
 	}
 }
