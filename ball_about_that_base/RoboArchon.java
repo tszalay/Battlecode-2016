3664d8ef554a35b97d18efa6c8ab3efe42ff1c13
Tamas Szalay <tamas@seas.harvard.edu>
2016-01-10 20:16:20 -0500

    Archon state updates

--- ball_about_that_base/RoboArchon.java
+++ ball_about_that_base/RoboArchon.java
@@ -4,86 +4,138 @@ import java.util.ArrayList;
 
 import battlecode.common.*;
 
-//enum for encoding state
-enum State
-{
-	RALLYING,
-	BUILDING,
-	REPAIRING,
-	SEARCHING
-}
-
 public class RoboArchon extends RobotPlayer
-{	
+{
+	enum ArchonState
+	{
+		INIT,
+		RALLYING,
+		BUILDING,
+		WAYPOINT
+	}
+
 	static int robotSchedule[] = {10, 10, 10, 6}; // 10-turret 6 - scout
 	static int scheduleCounter = rand.nextInt(100);
 	
 	static final int MAX_ROUNDS_TO_RALLY = 200;
 	
-	static boolean arrivedAtRally = false;
-	static boolean builtMyScout = false;
-	
-	static State myState;
+	static ArchonState myState = ArchonState.INIT;
 
 	public static void init() throws GameActionException
 	{
-		myState = State.RALLYING;
 	}
 	
+	public static void turn() throws GameActionException
+	{
+		// state machine update
+		updateState();
+		Debug.setStringSJF(myState.toString());
+			
+		// do turn according to state
+		switch (myState)
+		{
+		case INIT:
+			doInit();
+			break;
+		case RALLYING:
+			doRally();
+			break;
+		case BUILDING:
+			doBuild();
+			break;
+		case WAYPOINT:
+			doWaypoint();
+			break;
+		}
 	
-//	public static void bull()
-//	{
-//		if (rc.getRoundNum() == RoboScout.SIGNAL_ROUND)
-//			Message.calcRallyLocation();
-//
-//		if(rc.getRoundNum()== 1)
-//		{
-//			if (tryBuildEven(RobotType.SCOUT)) 
-//			{
-//				Message.sendBuiltMessage();
-//			}
-//		}
-//		
-//		if (!Micro.tryAvoidBeingShot())
-//		{
-//			if (amIBeingAttacked())
-//			{
-//				Micro.updateAllies();
-//				// find closest turtle location, and move away
-//				NavSafetyPolicy safety = new SafetyPolicyAvoidAllUnits();
-//				if (rc.isCoreReady())
-//					Nav.goTo(Micro.getUnitCOM(Micro.nearbyAllies), safety);
-//			}
-//			else
-//			{
-//				if (!arrivedAtRally) 
-//				{
-//					arrivedAtRally = tryMoveNearRally();
-//				}
-//				else
-//				{
-//					nextTurretLoc = MapUtil.findClosestTurtle();
-//					
-//					// if it's too close to where i was last shot, change the nextTurretLoc
-//					//if (nextTurretLoc != null && locLastAttacked != null  && nextTurretLoc.distanceSquaredTo(locLastAttacked)<2 )
-//					//	nextTurretLoc = here.add(here.directionTo(locLastAttacked).opposite());
-//					
-//					if	(!tryBuildUnits(nextTurretLoc))
-//					{
-//						Debug.setStringAK("trying to move to turret dest" + nextTurretLoc);
-//						tryMoveNearTurretDest(nextTurretLoc);
-//					}
-//				}
-//			}
-//		}
-//		
-//		//repair anyone nearby
-//		tryRepair();
-//
-//	}
+		// always do this, no reason not to
+		tryRepair();
+		
+		Debug.setStringTS("D:" + MapInfo.zombieDenLocations.elements().size()
+				+ ",P:" + MapInfo.goodPartsLocations.elements().size());
+	}
 	
+	private static void updateState() throws GameActionException // this will probably need tweaking
+	{
+		switch (myState)
+		{
+		case INIT:
+			break;
+			
+		case BUILDING:
+			// keep going to the next location if we're done building
+			// (and send startup built messages)
+			if (rc.isCoreReady())
+			{
+				myState = ArchonState.WAYPOINT;
+				Message.sendBuiltMessage();
+			}
+			break;
+			
+			
+		case RALLYING:
+			if (arrivedAtRally() || rc.getRoundNum() > MAX_ROUNDS_TO_RALLY)
+				myState = ArchonState.WAYPOINT;			
+			break;
+		
+		case WAYPOINT:
+			if (canBuildNow())
+				myState = ArchonState.BUILDING;
+			break;			
+		}
+	}
 	
-
+	public static void doInit() throws GameActionException
+	{
+		tryBuildEven(RobotType.SCOUT);
+		myState = ArchonState.RALLYING;
+	}
+	
+	public static void doWaypoint() throws GameActionException
+	{
+		Behavior.tryGoToWithoutBeingShot(MapInfo.getClosestPart());
+	}
+	
+	public static void doRally() throws GameActionException
+	{
+		// update rally location
+		if (rc.getRoundNum() == RoboScout.SIGNAL_ROUND)
+			Message.calcRallyLocation();
+		
+		// rally
+		if (Message.rallyLocation == null)
+			return;
+		
+		Behavior.tryGoToWithoutBeingShot(Message.rallyLocation);
+	}
+	
+	private static void doBuild() throws GameActionException
+	{
+		RobotType nextRobotType = getNextBuildRobotType();
+		
+		// return false quickly if we cannot build for an obvious reason
+		if (nextRobotType == null)
+			return;
+		if (!rc.isCoreReady())
+			return;
+		if (!rc.hasBuildRequirements(nextRobotType))
+			return;
+		
+		// find okay direction set
+		DirectionSet canBuild = getCanBuildDirectionSet(nextRobotType);
+		DirectionSet parity = getParityAllowedDirectionSet(nextRobotType);
+		DirectionSet validBuildDirectionSet = canBuild.and(parity);
+		
+		// other considerations like safety can be considered here
+		
+		// pick one of the allowed directions at random
+		Direction dir = validBuildDirectionSet.getRandomValid();
+		
+		// convert to directions and try to build
+		tryBuildUnit(nextRobotType, dir);
+		return;
+	}
+	
 	public static boolean tryRepair() throws GameActionException
 	{
 		RobotInfo[] nearbyFriends = rc.senseNearbyRobots(rc.getType().attackRadiusSquared,ourTeam);
@@ -258,190 +310,11 @@ public class RoboArchon extends RobotPlayer
 		return (validBuildDirectionSet.any());
 	}
 	
-	public static boolean canRepair() throws GameActionException
+	private static boolean arrivedAtRally()
 	{
-		RobotInfo[] nearbyFriends = rc.senseNearbyRobots(rc.getType().attackRadiusSquared,ourTeam);
-		RobotInfo minBot = null;
-
-		for (RobotInfo ri : nearbyFriends)
-		{
-			if (ri.type == RobotType.ARCHON)
-				continue;
-
-			if (minBot == null || (ri.maxHealth - ri.health) > (minBot.maxHealth - minBot.health) || minBot.zombieInfectedTurns < ri.zombieInfectedTurns)
-				minBot = ri;
-		}
-
-		if (minBot != null && minBot.health < minBot.maxHealth-1)
-			return true;
-		
-		return false;
-	}
-	
-	public static void doSearch() throws GameActionException
-	{
-		MicroBase micro = new MicroBase();
-		DirectionSet safeDirs = micro.getSafeMoveDirs();
-		ArrayList<Direction> safeDirArray = safeDirs.getDirections();
-		
-		return;
-		
-		// look for nearby neutrals and parts
-		/*
-		MapLocation[] parts = new MapLocation[0];// Message.partsLocs.loc;  // WANTED
-		MapLocation[] neutrals = new MapLocation[0]; //Message.neutralsLocs.loc;  // WANTED
-		
-		// move toward closest
-		MapLocation closest = null;
-		if (neutrals != null && neutrals.length > 0)
-		{
-			for (MapLocation loc : neutrals)
-			{
-				if (closest == null || here.distanceSquaredTo(loc) < here.distanceSquaredTo(closest))
-					closest = loc;
-			}
-		}
-		if (closest == null && parts != null && parts.length > 0)
-		{
-			for (MapLocation loc : parts)
-			{
-				if (closest == null || here.distanceSquaredTo(loc) < here.distanceSquaredTo(closest))
-					closest = loc;
-			}
-		}
-		
-		Nav.tryAdjacentSafeMove(here.directionTo(closest), safeDirs);*/
-	}
-	
-	public static void doRally() throws GameActionException
-	{
-		// update rally location
-		if (rc.getRoundNum() == RoboScout.SIGNAL_ROUND)
-			Message.calcRallyLocation();
-		
-		// make sure i built one scout
-		if (!builtMyScout)
-		{
-			builtMyScout = tryBuildEven(RobotType.SCOUT);
-		}
-		
-		// rally
 		if (Message.rallyLocation == null)
-			return;
+			return false;
 		
-		if (here.distanceSquaredTo(Message.rallyLocation) < 20)
-		{
-			arrivedAtRally = true;
-			return;
-		}
-		else
-		{
-			
-			MicroBase micro = new MicroBase();
-			Nav.tryGoTo(Message.rallyLocation, micro.getSafeMoveDirs());
-		}	
-	}
-	
-	private static void doBuild() throws GameActionException
-	{
-		RobotType nextRobotType = getNextBuildRobotType();
-		
-		// return false quickly if we cannot build for an obvious reason
-		if (nextRobotType == null)
-			return;
-		if (!rc.isCoreReady())
-			return;
-		if (!rc.hasBuildRequirements(nextRobotType))
-			return;
-		
-		// find okay direction set
-		DirectionSet canBuild = getCanBuildDirectionSet(nextRobotType);
-		DirectionSet parity = getParityAllowedDirectionSet(nextRobotType);
-		DirectionSet validBuildDirectionSet = canBuild.and(parity);
-		
-		// other considerations like safety can be considered here
-		
-		// pick one of the allowed directions at random
-		Direction dir = validBuildDirectionSet.getRandomValid();
-		
-		// convert to directions and try to build
-		tryBuildUnit(nextRobotType, dir);
-		return;
-	}
-	
-	public static void doRepair() throws GameActionException
-	{
-		tryRepair();
-	}
-	
-	private static void updateState() throws GameActionException // this will probably need tweaking
-	{
-		// fact 0: if we are mid-building, we are still building
-		if (myState == State.BUILDING && !rc.isCoreReady())
-		{
-			return;
-		}
-		
-		// priority 1: go to rally
-		if (!arrivedAtRally && rc.getRoundNum() < MAX_ROUNDS_TO_RALLY)
-		{
-			myState = State.RALLYING;
-			return;
-		}
-		
-		// priority 2: repair bots
-		if (canRepair())
-		{
-			myState = State.REPAIRING;
-			return;
-		}
-		
-		// priority 3: if we can build, build
-		if (canBuildNow()) // problem with this logic flow is i am calculating where to build twice...
-		{
-			myState = State.BUILDING;
-			return;
-		}
-			
-		// priority 4: search for stuff
-		myState = State.SEARCHING;
-		return;
-	}
-	
-	public static void turn() throws GameActionException
-	{
-		// avoiding taking damage is the top priority
-		if (!Micro.tryAvoidBeingShot())
-		{
-			// state machine update
-			updateState();
-			Debug.setStringSJF(myState.toString());
-			
-			if (rc.isCoreReady())
-			{
-				// do turn according to state
-				switch (myState)
-				{
-				case RALLYING:
-					doRally();
-					break;
-				case BUILDING:
-					doBuild();
-					break;
-				case REPAIRING:
-					doRepair();
-					break;
-				case SEARCHING:
-					doSearch();
-					break;
-				default:
-					System.out.println("Archon state machine is messed up.");
-					break;
-				}
-			}
-		}
-		
-		Debug.setStringTS("D:" + MapInfo.zombieDenLocations.elements().size()
-				+ ",P:" + MapInfo.goodPartsLocations.elements().size());
+		return here.distanceSquaredTo(Message.rallyLocation) < 20;
 	}
 }
