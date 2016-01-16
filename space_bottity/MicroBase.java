99deb3f65c3953a11c92052d1f5814862ee480c0
Tamas Szalay <tamas@seas.harvard.edu>
2016-01-15 20:52:46 -0500

    Target priority checking to micro

--- space_bottity/MicroBase.java
+++ space_bottity/MicroBase.java
@@ -545,40 +545,49 @@ public class MicroBase extends RobotPlayer
 		
 		return target;
 	}
-
 	
-//	public boolean canWin1v1(RobotInfo enemy) throws GameActionException
-//	{
-//		MapLocation onecloser = here.add(here.directionTo(enemy.location));
-//		
-//		boolean iAmAlive = true;
-//		boolean theyAreAlive = false;
-//		
-//		MapLocation myLoc = here;
-//		MapLocation enemyLoc = enemy.location;
-//		
-//		double myCoreDelay = rc.getCoreDelay();
-//		double theirCoreDelay = enemy.coreDelay;
-//		double myWeaponDelay = rc.getWeaponDelay();
-//		double theirWeaponDelay = enemy.weaponDelay;
-//		
-//		while (iAmAlive && theyAreAlive)
-//		{
-//			// me
-//			if (myCoreDelay < 1)
-//				myLoc
-//			
-//			// enemy
-//			
-//			
-//		}
-//		
-//		int numRoundsTillIKillThem = ;
-//		int numRoundsTillTheyKillMe = ;
-//		
-//		if (numRoundsTillIKillThem < numRoundsTillTheyKillMe)
-//			return true;
-//		
-//		return false;
-//	}
+	public RobotInfo getHighestPriorityTarget(RobotInfo[] bots)
+	{
+		RobotInfo target = null;
+		
+		for (RobotInfo ri : bots)
+		{
+			// can't attack this dude
+			if (here.distanceSquaredTo(ri.location) > rc.getType().attackRadiusSquared)
+				continue;
+			
+			// no target yet
+			if (target == null)
+			{
+				target = ri;
+				continue;
+			}
+			
+			// archon targets take priority over ALL
+			if (ri.type == RobotType.ARCHON)
+			{
+				target = ri;
+				break;
+			}
+			
+			double power = ri.attackPower/ri.type.attackDelay;
+			double tgtpower = target.attackPower/target.type.attackDelay;
+			// ri's damage/turn is less than current target, do not prefer
+			if (power < tgtpower - 1e-8)
+				continue;
+			
+			// greater than current, do prefer
+			if (power > tgtpower + 1e-8)
+			{
+				target = ri;
+				continue;
+			}
+		
+			// they are the same, compare health
+			if (ri.health < target.health)
+				target = ri;
+		}
+		
+		return target;
+	}
 }
