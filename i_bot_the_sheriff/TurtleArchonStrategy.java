0600ebd43efda80b4f9cf4beb4f6a6c57e82e1f3
Tamas Szalay <tamas@seas.harvard.edu>
2016-01-16 14:50:54 -0500

    Added unit counting code

--- i_bot_the_sheriff/TurtleArchonStrategy.java
+++ i_bot_the_sheriff/TurtleArchonStrategy.java
@@ -4,36 +4,52 @@ import battlecode.common.*;
 
 import java.util.*;
 
-import ball_me_maybe.MapInfo;
-
 // turtle move tries to allow units to move around close to or inside a turtle ball
 public class TurtleArchonStrategy extends RobotPlayer implements Strategy
 {
 	private MapLocation turtleLocation = null;
+	private Strategy overrideStrategy = null;
 	
 	public TurtleArchonStrategy()
 	{
 		// find a good turtle location
 		// for now, pick a random corner of the map
 		turtleLocation = new MapLocation(
-					rand.nextBoolean() ? MapInfo.mapMin.x : MapInfo.mapMax.x,
-					rand.nextBoolean() ? MapInfo.mapMin.y : MapInfo.mapMax.y
-				);
+						rand.nextBoolean() ? MapInfo.mapMin.x : MapInfo.mapMax.x,
+						rand.nextBoolean() ? MapInfo.mapMin.y : MapInfo.mapMax.y
+					);
 	}
 	
 	public boolean tryTurn() throws GameActionException
 	{
-		switch (rc.getType())
+		// do we have a strategy that takes precedence over this one?
+		// (e.g. combat or building)
+		if (overrideStrategy != null)
 		{
-		case TURRET:
-			break;
-		case TTM:
-			break;
-		case SCOUT:
-			break;
-		case SOLDIER:
-			break;
+			if (overrideStrategy.tryTurn())
+				return true;
+			else
+				overrideStrategy = null;
 		}
+		
+		// if we're far from the closest corner, let's go there
+		if (MapInfo.closestCornerDistanceSq() > 53)
+		{
+			Nav.tryGoTo(turtleLocation, Micro.getSafeMoveDirs());
+			return true;
+		}
+		
+		// are we in any danger?
+		if (Micro.getRoundsUntilDanger() < 10)
+		{
+			Action.tryRetreatTowards(Micro.getAllyCOM(), Micro.getSafeMoveDirs());
+			return true;
+		}
+		
+		// count number of units nearby to decide what to build
+		
+		
+			
 		return true;
 	}
 }
