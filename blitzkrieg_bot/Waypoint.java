7b3b9250bf944d27a529dbe3adee99e222254867
Tamas Szalay <tamas@seas.harvard.edu>
2016-01-24 16:43:23 -0500

    Adding target stores to waypoint

--- blitzkrieg_bot/Waypoint.java
+++ blitzkrieg_bot/Waypoint.java
@@ -6,6 +6,82 @@ import java.util.*;
 
 public class Waypoint extends RobotPlayer
 {
+	// subclass for target round info
+	private static class TargetInfo
+	{
+		public int 			round;
+		public int 			value;
+		public MapLocation 	location;
+		
+		public TargetInfo(MapLocation loc, int val)
+		{
+			round = rc.getRoundNum();
+			value = val;
+			location = loc;
+		}
+	}
+	
+	private static class TargetStore
+	{
+		public TargetInfo[] targets = new TargetInfo[5];
+		private final int timeout;
+		
+		public TargetStore(int time)
+		{
+			timeout = time;
+		}
+		
+		public void add(TargetInfo ti)
+		{
+			int oldestInd = 0;
+			int lowestInd = 0;
+			
+			for (int i=0; i<targets.length; i++)
+			{
+				if (targets[i] == null)
+				{
+					targets[i] = ti;
+					return;
+				}
+				if (targets[i].round < targets[oldestInd].round)
+					oldestInd = i;
+				if (targets[i].value < targets[lowestInd].value)
+					lowestInd = i;
+			}
+			// if we have a value higher than the lowest value, automatically replace it
+			if (ti.value > targets[lowestInd].value)
+			{
+				targets[lowestInd] = ti;
+				return;
+			}
+			// or if the oldest one has timed out
+			if (roundsSince(oldestInd) > timeout)
+			{
+				targets[oldestInd] = ti;
+				return;
+			}
+		}
+
+		public MapLocation getClosestRecent()
+		{
+			return getClosestRecent(timeout);
+		}
+		
+		public MapLocation getClosestRecent(int timelimit)
+		{
+			MapLocation loc = null;
+			
+			for (int i=0; i<targets.length; i++)
+			{
+				if (targets[i] == null)
+					continue;
+				if (loc == null || here.distanceSquaredTo(targets[i].location) < here.distanceSquaredTo(loc))
+					loc = targets[i].location;
+			}
+			return loc;
+		}
+	}
+	
 	private static MapLocation randomDest = null;
 	private static int randomDestRound = -10000;
 	
