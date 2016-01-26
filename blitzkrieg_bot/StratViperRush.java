833708efeb50bbaeb2f535fe4687fe109572571b
Tamas Szalay <tamas@seas.harvard.edu>
2016-01-26 15:21:40 -0500

    Stephen's old viper rush is back

--- blitzkrieg_bot/StratViperRush.java
+++ blitzkrieg_bot/StratViperRush.java
@@ -10,6 +10,9 @@ public class StratViperRush extends RobotPlayer implements Strategy
 	
 	private MapLocation lastDest = null;
 	private MapLocation startingLoc = null;
+	private MapLocation enemyLoc = null;
+	private MapLocation farArchon = null;
+	private MapLocation[] enemyArchonLocs = null;
 	
 	public String getName()
 	{
@@ -21,6 +24,9 @@ public class StratViperRush extends RobotPlayer implements Strategy
 	
 	public StratViperRush()
 	{
+		enemyArchonLocs = rc.getInitialArchonLocations(theirTeam);
+		enemyLoc = Micro.getClosestLocationTo(enemyArchonLocs,here);
+		farArchon = Micro.getFarthestLocationFrom(enemyArchonLocs,here);
 		startingLoc = here;
 	}
 	
@@ -36,105 +42,17 @@ public class StratViperRush extends RobotPlayer implements Strategy
 		}
 		
 		// shoot self if almost dead
-		if (rc.isWeaponReady() && rc.getHealth() <= Micro.getNearbyAllies().length * 13 * rc.getWeaponDelay() 
+		if (rc.isWeaponReady() && ((rc.getHealth() <= Micro.getNearbyAllies().length * 13 * rc.getWeaponDelay() 
 				&& rc.getInfectedTurns() < rc.getHealth()/(13*Micro.getNearbyAllies().length))
+				|| Micro.getEnemyUnits().Turrets > 0))
 			rc.attackLocation(here);
 		
 		// attack
 		// (after round 200, rushing vipers will shoot anything)
-		Action.tryViperAttack();
-		
-		// do we have a strategy that takes precedence over this one?
-		if (overrideStrategy != null)
-		{
-			if (overrideStrategy.tryTurn())
-				return true;
-			else
-				overrideStrategy = null;
-		}
-		
-		// any vipers or turrets? rush 'em
-		if (Micro.getEnemyUnits().TurrTTMs > 0)
-		{
-			for (RobotInfo ri : Micro.getNearbyEnemies())
-			{
-				if (ri.type == RobotType.TURRET || ri.type == RobotType.TTM)
-				{
-					overrideStrategy = new StratTempRush(ri.ID);
-					overrideStrategy.tryTurn();
-					return true;
-				}
-			}
-		}
-		
-		// rush targets are visible enemies, then enemy locations, then random
-		lastDest = null;
-		RobotInfo closestUnit = Micro.getClosestUnitTo(Micro.getNearbyEnemies(), here);
-		if (closestUnit != null)
-			lastDest = closestUnit.location;
-
-		if (lastDest == null)
-			lastDest = Waypoint.getBestEnemyLocation();
-
-		if (lastDest == null)
-			lastDest = Waypoint.getRandomRetreatWaypoint();
-		
-		
-		// use buffer dirs (so we can shoot enemies) if there are uninfected enemies and not too many of them
-		DirectionSet bufferDirs = Micro.getBufferDirs();
-		bufferDirs = bufferDirs.and(Micro.getTurretSafeDirs());
-		
-		/*
-		int numuninfected = 0;
-		int numcanattack = 0;
-		for (RobotInfo ri : rc.senseNearbyRobots(rc.getType().attackRadiusSquared, theirTeam))
-		{
-			if (ri.viperInfectedTurns < 3)
-				numuninfected++;
-			if (ri.type.canAttack())
-				numcanattack++;
-		}
-		
-		// too many units or nobody to shoot, try to move safely
-		if (numuninfected == 0 || numcanattack > 2 || !bufferDirs.any())
-		{
-			// try to move safely
-			if (Action.tryGoToSafestOrRetreat(lastDest))
-				return true;
-		}
-		*/
-
-		// otherwise shoot
-		Action.tryViperAttack();
-		
-		// and then buffer move
-		if (Micro.getNearbyHostiles() != null && Micro.getNearbyHostiles().length > 0)
-			Nav.tryGoTo(lastDest, bufferDirs);
-		else
-			Nav.tryGoTo(lastDest, Micro.getCanMoveDirs()); // dig and move as fast as possible to enemy, don't bug like a jackass
-		
-		// if overpowered, kite back
-		//if (Micro.amOverpowered())
-		//	Action.tryRetreatOrShootIfStuck();
-		
-		// shoot if we're safe here
-		/*
-		if (bufferDirs.isValid(Direction.NONE) && Action.tryAttackSomeone())
-			return true;
-		
-		// don't move if we're safe and just shot
-		if (bufferDirs.isValid(Direction.NONE) && !rc.isWeaponReady())
-		{
-			if (Micro.getRoundsUntilDanger() > 10)
-			{
-				Direction d = bufferDirs.getDirectionTowards(here,lastDest);
-				if (d != null)
-					Action.tryMove(d);
-			}
-			return true;
-		}
-		*/
-        /*RobotInfo[] enemies = Micro.getNearbyEnemies();
+		if (rc.getRoundNum() > 200 || here.distanceSquaredTo(enemyLoc) < here.distanceSquaredTo(startingLoc))
+			Action.tryViperAttack();
+        
+        RobotInfo[] enemies = Micro.getNearbyEnemies();
         
         // rush turrets to get so close they can't shoot
         UnitCounts count = Micro.getEnemyUnits();
@@ -151,7 +69,9 @@ public class StratViperRush extends RobotPlayer implements Strategy
         		}
         	}
         	if (enemyturretloc != null)
+        	{
         		Nav.tryGoTo(enemyturretloc, Micro.getCanMoveDirs());
+        	}
         }
         
         // try to have only one enemy in attack range at a time
@@ -204,10 +124,7 @@ public class StratViperRush extends RobotPlayer implements Strategy
         else
         	Nav.tryGoTo(enemyLoc, Micro.getBestSafeDirs());
         
-        return true;*/
-
-		
-		return true;
+        return true;
 
 	}
 }
